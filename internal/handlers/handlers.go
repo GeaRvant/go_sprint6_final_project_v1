eb7bf5e10f108c9a692c35c776a29ab25bf18fbf
Anatoly Vrublevskikh <vrublevskikh.anatolij@yandex.ru>
2025-11-03 22:37:03 +0300

    Another attempt to defeat autotests.

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -13,7 +13,8 @@ import (
 
 func FirstHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	http.ServeFile(w, r, filepath.Join("..", "index.html"))
+	//http.ServeFile(w, r, filepath.Join("..", "index.html"))
+	http.ServeFile(w, r, filepath.Join(`D:\Study\go_sprint6_final_project_v1\index.html`))
 }
 
 func SecondHandler(w http.ResponseWriter, r *http.Request) {
