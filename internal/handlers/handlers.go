4d27282e8c745520fbab506aeafc6caf69e8aa74
Anatoly Vrublevskikh <vrublevskikh.anatolij@yandex.ru>
2025-11-03 22:11:16 +0300

    The task is complete. The service, server, handler, and main packages have been implemented. The program has been tested successfully.

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,72 @@
 package handlers
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func FirstHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, filepath.Join("..", "index.html"))
+}
+
+func SecondHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "некорректный метод", http.StatusMethodNotAllowed)
+		return
+	}
+
+	if err := r.ParseMultipartForm(10 << 20); err != nil {
+		http.Error(w, "некорректный парсинг формы", http.StatusBadRequest)
+		return
+	}
+
+	file, _, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, "невозможно полкчить файл из формы", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	content, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "невозможно прочитать файл", http.StatusInternalServerError)
+		return
+	}
+
+	result, err := service.Convert(string(content))
+	if err != nil {
+		http.Error(w, "некорректная конвертация: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	filename := fmt.Sprintf("%s%s", time.Now().UTC().Format("20060102T150405"), filepath.Ext("index.html"))
+
+	outFile, err := os.Create(filename)
+	if err != nil {
+		http.Error(w, "невозможно создать файл", http.StatusInternalServerError)
+		return
+	}
+	defer outFile.Close()
+
+	if _, err := outFile.WriteString(result); err != nil {
+		http.Error(w, "невозможно записать файл", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	response := fmt.Sprintf(`
+<html>
+<head><title>Результат загрузки</title></head>
+<body>
+<p>Конвертация завершена.</p>
+<p>Результат записан в файл: <strong>%s</strong></p>
+</body>
+</html>`, filename)
+	w.Write([]byte(response))
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,48 @@
 package service
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+var conv = morse.DefaultConverter
+
+func Convert(input string) (string, error) {
+	input = strings.TrimSpace(input)
+	if len(input) == 0 {
+		return "", fmt.Errorf("входная строка пуста")
+	}
+	isMorse, err := isMorseCode(input)
+	if err != nil {
+		return "", err
+	}
+	if isMorse {
+		result := conv.ToText(input)
+		if result == "" {
+			return "", fmt.Errorf("не удалось преобразовать морзе в текст")
+		}
+		fmt.Println("преобразование из морзе успешно")
+		return result, nil
+	} else {
+		result := conv.ToMorse(input)
+		if result == "" {
+			return "", fmt.Errorf("не удалось преобразовать текст в морзе")
+		}
+		fmt.Println("преобразование в морзе успешно")
+		return result, nil
+	}
+}
+
+func isMorseCode(s string) (bool, error) {
+	if len(s) == 0 {
+		return false, fmt.Errorf("пустая строка")
+	}
+	for _, ch := range s {
+		if !(ch == '.' || ch == '-' || ch == ' ' || ch == '/') {
+			return false, nil
+		}
+	}
+	return true, nil
+}
